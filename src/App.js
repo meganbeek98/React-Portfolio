5e88b74e289cd964638c3e2ea1200019b82491ee
meganbeek98 <meganbeek98@gmail.com>
2022-07-04 11:42:37 -0400

    import About, Contact, Navbar, Projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import About from "./Layout/About";
+import Contact from "./Layout/Contact";
+import Navbar from "./Layout/Navbar";
+import Projects from "./Layout/Projects";
 
 export default function App() {
   return (
