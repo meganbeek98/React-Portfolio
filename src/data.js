d4ec7d91c506ce89d3e32201c2e21f37e635e432
meganbeek98 <meganbeek98@gmail.com>
2022-07-04 12:51:14 -0400

    added placeholders to add projects to portfolio

---
+++ src/data.js
@@ -0,0 +1,35 @@
+// Each Project's Individual Info / Element
+export const projects = [
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
+        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+    },
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
+        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+    },
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
+        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+    },
+    {
+        title: "Placeholder",
+        subtitle: "Placeholder",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
+        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+    },
+  ];
