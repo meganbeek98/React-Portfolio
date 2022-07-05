c06c83114bbbdcfaeb3e36b18f595617ede618e0
meganbeek98 <meganbeek98@gmail.com>
2022-07-04 22:50:40 -0400

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,14 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                    Hi, I'm Megan.
-                    <br className="hidden lg:inline-block" />I am a web developer, still in her learning phases. But, I aspire and strive for greatness in everything I do in life.
+                    <i>"Hello World!"</i> ♥︎ <br/> <br/> My name is, <br/> Megan Beekman.
+                    <br/> 
+                    <br className="hidden lg:inline-block" />
                 </h1>
                 <p className="mb-8 leading-relaxed">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                    laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                    Laborum, voluptas natus?
+                    I am a woman in Web Development. ⌨ &#9792;
+                Although I'm relatively new to the industry,<br></br> I have always aspired and strived for greatness in everything I do in life. I am a hard-worker. <br></br>
+                And I will work hard all throught this career and continue to master my skills everyday. Momma didn't raise a quitter! ᕦ(ò_óˇ) 
                 </p>
                 <div className="flex justify-center">
                     <a
