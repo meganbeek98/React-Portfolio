2bcb9a5b671bd53a24876f999f8a742866ea95b5
meganbeek98 <meganbeek98@gmail.com>
2022-07-04 23:07:42 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -30,11 +30,11 @@ export default function About() {
                 </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <img
-                    className="object-cover object-center rounded"
-                    alt="hero"
-                    src="" // ADD headshot or image later
-                />
+                    <img
+                        className="object-cover object-center rounded"
+                        alt="hero"
+                        src="" // ADD headshot or image later
+                    />
                 </div>
             </div>
         </section>
