53d3a3e836a52f950116f9075d9c46103a85665f
meganbeek98 <meganbeek98@gmail.com>
2022-07-08 01:24:57 -0400

    added PassGen to projects list

--- src/data.js
+++ src/data.js
@@ -1,12 +1,11 @@
 // Each Project's Individual Info / Element
 export const projects = [
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
-        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+        title: "PassGen 2.0",
+        subtitle: "A Secure Password Generator",
+        description: " A simple application that can be used to generate secure and completely random passwords that are dependent on the user's declared parameters.",
+        image: "./assets/", // add image(.img, or .svg) or gif(.gif) for project
+        link: "/src/projects-images/SS-passGen.png", // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
     {
         title: "Placeholder",
