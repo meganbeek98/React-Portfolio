a39edefbcf9f141228f5a095af1dfc85768399d9
meganbeek98 <meganbeek98@gmail.com>
2022-07-08 01:36:44 -0400

    added images

--- src/data.js
+++ src/data.js
@@ -3,21 +3,22 @@ export const projects = [
     {
         title: "PassGen 2.0",
         subtitle: "A Secure Password Generator",
-        description: " A simple application that can be used to generate secure and completely random passwords that are dependent on the user's declared parameters.",
+        description: 
+            " A simple application that can be used to generate secure and completely random passwords that are dependent on the user's declared parameters.",
         image: "./assets/", // add image(.img, or .svg) or gif(.gif) for project
         link: "/src/projects-images/SS-passGen.png", // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
+        title: "'Weather or not, here I come..!'",
+        subtitle: "A Weather Dashboard Application",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
+            "A simple weather dashboard that displays weather condtions depending on the city the user types into the search box.",
+        image: "./projects-images/SS-weather-dash.png", // add image(.img, or .svg) or gif(.gif) for project
         link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
+        title: "Javascript Quiz (Level: Junior)",
+        subtitle: "A timed, multiple choice quiz that begins on click of the 'start' button",
         description:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
         image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
