c37cf44633eda970f63bca5c92d4d411b1f2120a
meganbeek98 <meganbeek98@gmail.com>
2022-07-08 02:26:10 -0400

    working through image rendering issues

--- src/data.js
+++ src/data.js
@@ -3,33 +3,29 @@ export const projects = [
     {
         title: "PassGen 2.0",
         subtitle: "A Secure Password Generator",
-        description: 
-            " A simple application that can be used to generate secure and completely random passwords that are dependent on the user's declared parameters.",
-        image: "./assets/", // add image(.img, or .svg) or gif(.gif) for project
-        link: "/src/projects-images/SS-passGen.png", // insert link to project (i.e., Deployment and/or GitHub Repo)
+        description:" A simple application that can be used to generate secure and completely random passwords that are dependent on the user's declared parameters.",
+        image: "./components/SS-passGen.png", // add image(.img, or .svg) or gif(.gif) for project
+        link: "https://meganbeek98.github.io/password-generator/" // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
     {
         title: "'Weather or not, here I come..!'",
         subtitle: "A Weather Dashboard Application",
-        description:
-            "A simple weather dashboard that displays weather condtions depending on the city the user types into the search box.",
+        description:"A simple weather dashboard that displays weather condtions depending on the city the user types into the search box.",
         image: "./projects-images/SS-weather-dash.png", // add image(.img, or .svg) or gif(.gif) for project
-        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+        link: "https://meganbeek98.github.io/weather-dashboard/" // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
     {
         title: "Javascript Quiz (Level: Junior)",
-        subtitle: "A timed, multiple choice quiz that begins on click of the 'start' button",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
-        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+        subtitle: "A Quiz; Based on JavaScript",
+        description: "A junior-developer-level, multiple-choice quiz featuring questions about the fundamentals of Javascript.",
+        image: "./components/SS-Quiz.png", // add image(.img, or .svg) or gif(.gif) for project
+        link: "https://meganbeek98.github.io/code-quiz-2/" // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
     {
-        title: "Placeholder",
-        subtitle: "Placeholder",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./PLACEHOLDER", // add image(.img, or .svg) or gif(.gif) for project
-        link: "", // insert link to project (i.e., Deployment and/or GitHub Repo)
+        title: "'ELECTRIVIA' (in development/not complete)",
+        subtitle: "An app to Explore, Play, Create, and Share knowledge via Trivia Quizzes.",
+        description: "An app (still in development), where you can Explore, Play, Create, and Share many different Trivia Quizes. Compete globally with others, and/or simply complete quizes locally to earn yourself a spot on Electrivia's Leaderboard!",
+        image: "./components/SS-trivia-header.png",
+        link: "https://github.com/meganbeek98/trivia" // insert link to project (i.e., Deployment and/or GitHub Repo)
     },
   ];
